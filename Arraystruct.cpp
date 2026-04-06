d70572e71503263476df1b09e1670ca26494e8fc
faizfalahh <faizfalah38@gmail.com>
2026-04-06 16:28:41 +0800

    arraystruct3

--- Arraystruct.cpp
+++ Arraystruct.cpp
@@ -23,4 +23,15 @@ int main() {
     cin >> kamu[3];
     cout << "data index 4 = ";
     cin >> kamu[4];
+    cout << endl;
+    // menampilkan satu persatu
+    cout << "Data pertama = " << kamu[0] << endl;
+    cout << "Data kedua = " << kamu[1] << endl;
+    cout << "Data ketiga = " << kamu[2] << endl;
+    cout << "Data keempat = " << kamu[3] << endl;
+    cout << "Data kelima  = " << kamu[4] << endl;
+}
+}
+}
+}
 }
