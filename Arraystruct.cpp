e2b39c5328a5f84688fed98c2e941f59907e4402
faizfalahh <faizfalah38@gmail.com>
2026-04-06 16:12:32 +0800

    arraystruct1

---
+++ Arraystruct.cpp
@@ -0,0 +1,11 @@
+#include <iostreeam>
+using namespace std;
+
+int main() {
+    // deklarasi dengan inisialisasi
+    int aku{5} =c{10,20,30,40,50};
+    // deklarasi array tanpa inisialisasi
+    int kamu{5};
+    // tampilkan data
+    cout << "Data pada index 1 = " << aku{1} << endl;
+}
