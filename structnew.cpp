a8630b7fa5ca2bf904ed12af1d51ff644195863b
faizfalahh <faizfalah38@gmail.com>
2026-04-06 16:58:50 +0800

    arraystruct6

--- structnew.cpp
+++ structnew.cpp
@@ -8,3 +8,18 @@ struct orang
     int umur;
 };
 
+int main() {
+    orang mhs;
+    cout << "nama : " ;
+    cin >> mhs.nama;
+    cout << "alamat : " ;
+    cin >> mhs.alamat;
+    cout << "umur : " ;
+    cin >> mhs.umur;
+    // tampilkan
+    cout << " nama : " << mhs.nama << endl;
+    cout << " alamat : " << mhs.alamat << endl;
+    cout << " umur : " << mhs.umur << endl;
+
+
+}
