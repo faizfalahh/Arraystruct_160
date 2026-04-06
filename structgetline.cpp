bfe224d5d9f064737b608f22e47d0c44f72734e1
faizfalahh <faizfalah38@gmail.com>
2026-04-06 17:45:19 +0800

    arraystruct9

---
+++ structgetline.cpp
@@ -0,0 +1,35 @@
+#include <iostream>
+using namespace std;
+
+struct detailAlamat
+{
+    string desa;
+    string kota;
+};
+
+struct orang
+{
+    string nama;
+    detailAlamat alamat;
+    int umur;
+};
+
+int main() {
+    orang mhs[3];
+
+    for (int i = 0; i <= 2; i++)
+    {
+        cout << "nama : " ;
+        getline(cin, mhs[i].nama);
+        cout << "alamat desa : " ;
+        getline(cin, mhs[i].alamat.desa);
+        cout << "alamat kota : " ;
+        getline(cin, mhs[i].alamat.kota);
+        cout << "umur : " ;
+        cin >> mhs[i].umur;
+        cout << endl;
+        cin.ignore();
+
+    }
+
+}
